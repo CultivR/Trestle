12f5ef59f0feba67b3792b06f22ec41cb6a4a4d0
Jordan Kay <jordan.kay@albertsons.com>
2018-12-07 11:36:58 -0800

    WIP

--- Trestle/Dependencies.swift
+++ Trestle/Dependencies.swift
@@ -6,8 +6,6 @@
 //  Copyright © 2018 Cultivr. All rights reserved.
 //
 
-@_exported import Cipher
 @_exported import Lilt
 @_exported import Province
 @_exported import Tangram
-@_exported import UIKit
